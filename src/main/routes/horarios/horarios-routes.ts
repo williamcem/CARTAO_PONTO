b908cc5b6675f4364eb23430b6cd1775b281906e
William <william@inovaetech.com.br>
2024-04-10 10:56:00 -0300

    correção de rota

--- src/main/routes/horarios/horarios-routes.ts
+++ src/main/routes/horarios/horarios-routes.ts
@@ -2,7 +2,8 @@ import { Router } from "express";
 import { makeHorariosController } from "../../factories/horarios";
 import { adaptRoute } from "../../adapters/express-route-adapter";
 
-export default (router: Router): void => {
+const route = (router: Router): void => {
   router.post("/semana", adaptRoute(makeHorariosController()));
-  console.log(router.stack)
 };
+
+export default route;
