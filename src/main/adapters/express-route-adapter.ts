2a668a710baadd1092484fdcfb92a0b97b9fa798
William <william@inovaetech.com.br>
2024-04-10 10:56:54 -0300

    tirando aquivos desnecessários

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -1,6 +1,7 @@
 import { Controller } from "../../presentation/protocols/controller";
 import { Request, Response } from "express";
 import { HttpRequest } from "../../presentation/protocols/http";
+import axios from "axios";
 
 export const adaptRoute = (controller: Controller) => {
   return async (req: Request, res: Response) => {
