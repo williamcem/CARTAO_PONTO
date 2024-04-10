efc51710276d17a2abcc9bd7c78b8dad9b605931
William <william@inovaetech.com.br>
2024-04-10 10:50:22 -0300

    inserção de novos dados paara o banco

--- src/infra/db/postgresdb/horarios-repository/horarios.ts
+++ src/infra/db/postgresdb/horarios-repository/horarios.ts
@@ -11,26 +11,34 @@ export class HorariosPostgresRepository implements AddHorariosRepository {
   }
 
   async add(horarioData: AddHorariosModel): Promise<HorariosModel> {
+    // Salvar dados do horário no banco de dados
     const insertHorarios = await this.prisma.dia.create({
       data: {
+        id: horarioData.id,
+        data: horarioData.data,
         entradaManha: horarioData.entradaManha,
         saidaManha: horarioData.saidaManha,
         entradaTarde: horarioData.entradaTarde,
         saidaTarde: horarioData.saidaTarde,
+        entradaExtra: horarioData.entradaExtra,
+        saidaExtra: horarioData.saidaExtra,
         dif_min: horarioData.dif_min,
-        tipoUm: horarioData.tipoUm,
-        tipoDois: horarioData.tipoDois,
+        saldoAnt: horarioData.saldoAnt,
       },
     });
 
+    // Retornar modelo de horários
     const HorariosModel: HorariosModel = {
+      id: insertHorarios.id,
+      data: insertHorarios.data,
       entradaManha: insertHorarios.entradaManha,
       saidaManha: insertHorarios.saidaManha,
       entradaTarde: insertHorarios.entradaTarde,
       saidaTarde: insertHorarios.saidaTarde,
+      entradaExtra: insertHorarios.entradaExtra || undefined,
+      saidaExtra: insertHorarios.saidaExtra || undefined,
       dif_min: insertHorarios.dif_min,
-      tipoUm: insertHorarios.tipoUm,
-      tipoDois: insertHorarios.tipoDois,
+      saldoAnt: insertHorarios.saldoAnt,
     };
     return HorariosModel;
   }
