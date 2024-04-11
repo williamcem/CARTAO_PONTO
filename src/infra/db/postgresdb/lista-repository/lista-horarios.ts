68f9b99c9fab6516e1ba312c8a3abb124c7c0805
William <william@inovaetech.com.br>
2024-04-11 07:58:01 -0300

    reestrurando a funcionalidade listar

--- src/infra/db/postgresdb/lista-repository/lista-horarios.ts
+++ src/infra/db/postgresdb/lista-repository/lista-horarios.ts
@@ -1,14 +1,37 @@
-// list-horarios.ts
 import { PrismaClient } from "@prisma/client";
+import { ListarListaRepository } from "../../../../data/usecase/protocols/find-listarhorarios-repository";
+import { ListarListaModel } from "../../../../domain/usecases/lista-horarios";
+import { ListaModel } from "../../../../domain/models/lista";
 
-const prisma = new PrismaClient();
+export class ListaPostgresRepository implements ListarListaRepository {
+  private prisma: PrismaClient;
 
-export async function listHorarios() {
-  try {
-    const horarios = await prisma.dia.findMany({ orderBy: { data: "asc" } }); // Consulta todos os horários no banco de dados usando o Prisma
+  constructor() {
+    this.prisma = new PrismaClient();
+  }
 
-    return horarios; // Retorna os horários recuperados do banco de dados
-  } catch (error) {
-    throw new Error("Erro ao listar os horários"); // Lança um erro se ocorrer um problema ao acessar o banco de dados
+  public async list(horariosData: ListarListaModel): Promise<ListaModel[]> {
+    try {
+      // Consulta todos os horários no banco de dados usando o Prisma
+      const horarios = await this.prisma.dia.findMany({ orderBy: { data: "asc" } });
+
+      // Mapeia os horários recuperados do banco de dados para o formato desejado
+      const listaHorarios: ListaModel[] = horarios.map((horario) => ({
+        id: horario.id,
+        data: horario.data,
+        entradaManha: horario.entradaManha,
+        saidaManha: horario.saidaManha,
+        entradaTarde: horario.entradaTarde,
+        saidaTarde: horario.saidaTarde,
+        entradaExtra: horario.entradaExtra || undefined,
+        saidaExtra: horario.saidaExtra || undefined,
+        dif_min: horario.dif_min,
+        saldoAnt: horario.saldoAnt,
+      }));
+
+      return listaHorarios; // Retorna os horários recuperados e mapeados do banco de dados
+    } catch (error) {
+      throw new Error("Erro ao listar os horários"); // Lança um erro se ocorrer um problema ao acessar o banco de dados
+    }
   }
 }
--- src/main/factories/get-horarios.ts
+++ src/main/factories/get-horarios.ts
@@ -1,8 +1,10 @@
+import { ListaPostgresRepository } from "../../infra/db/postgresdb/lista-repository/lista-horarios";
 import { LogControllerDecorator } from "../decorators/log";
 import { Controller } from "../../presentation/protocols";
-import { getHorariosController } from "../../presentation/controllers/horarios/getHorarios";
+import { ListaHorariosController } from "../../presentation/controllers/lista/lista-horarios";
 
 export const makeListaHorarioController = (): Controller => {
-  const horariosController = new getHorariosController();
-  return new LogControllerDecorator(horariosController);
+  const listaPostgresRespository = new ListaPostgresRepository();
+  const listacontroller = new ListaHorariosController(listaPostgresRespository);
+  return new LogControllerDecorator(listacontroller);
 };
