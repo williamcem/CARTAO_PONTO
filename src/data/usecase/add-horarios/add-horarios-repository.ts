24ca0a36d1b38b2d66ce4472e421c7614cc0cedb
William <william@inovaetech.com.br>
2024-04-18 15:09:21 -0300

    implementação da função getSaldo

--- src/data/usecase/add-horarios/add-horarios-repository.ts
+++ src/data/usecase/add-horarios/add-horarios-repository.ts
@@ -1,5 +1,6 @@
 import { HorariosModel, AddHorariosModel } from "../../../presentation/controllers/horarios/horarios-protocols";
 
 export interface AddHorariosRepository {
+  getLastHorario: any;
   add(horariosData: AddHorariosModel): Promise<HorariosModel>;
 }
--- src/data/usecase/add-horarios/db-add-horarios.ts
+++ src/data/usecase/add-horarios/db-add-horarios.ts
@@ -1,6 +1,7 @@
 import { AddHorarios, AddHorariosModel } from "../../../domain/usecases/add-horarios";
 import { HorariosModel } from "../../../domain/models/horarios";
 import { AddHorariosRepository } from "./add-horarios-repository";
+import { HorarioData } from "../../../presentation/controllers/horarios/horarios"; // Certifique-se de importar HorarioData
 
 export class DbAddHorarios implements AddHorarios {
   private readonly addHorariosRepository: AddHorariosRepository;
@@ -10,7 +11,12 @@ export class DbAddHorarios implements AddHorarios {
   }
 
   async add(horariosData: AddHorariosModel): Promise<HorariosModel> {
-    const horarios = await this.addHorariosRepository.add(Object.assign({}, horariosData));
+    const horarios = await this.addHorariosRepository.add(horariosData);
     return horarios;
+  }
+
+  async getLastHorario(): Promise<HorarioData | null> {
+    const lastHorario = await this.addHorariosRepository.getLastHorario();
+    return lastHorario;
   }
 }
