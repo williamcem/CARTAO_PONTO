223044abff7107d493740e5d037730befa43dfff
William <william@inovaetech.com.br>
2024-04-18 15:30:07 -0300

    atualização de importação de aqruivos

--- src/main/factories/uplod-arquivos.ts
+++ src/main/factories/uplod-arquivos.ts
@@ -1,9 +1,7 @@
 import { Controller } from "../../presentation/protocols";
 import { LogControllerDecorator } from "../decorators/log";
-import { DbAddUpload } from "../../data/usecase/upload-protheus/db-add-upload-prothues";
 import { UploadPostgresRepository } from "../../infra/db/postgresdb/uplod-repository/upload-protheus";
 import { UploadController } from "../../presentation/controllers/upload-protheus/upload-prothues";
-import { processarArquivo } from "../adapters/protheus-route-adapter";
 
 export const makeUploadController = (): Controller => {
   const uploadPostgresRepository = new UploadPostgresRepository();
