d202a95ed6a0f36c09b210d9f72202d4d9b6a977
William <william@inovaetech.com.br>
2024-04-25 16:49:15 -0300

    correção ortográfica

--- src/main/factories/get-horarios.ts
+++ src/main/factories/get-horarios.ts
@@ -4,7 +4,7 @@ import { Controller } from "../../presentation/protocols";
 import { ListaHorariosController } from "../../presentation/controllers/lista/lista-horarios";
 
 export const makeListaHorarioController = (): Controller => {
-  const listaPostgresRespository = new ListaPostgresRepository();
-  const listacontroller = new ListaHorariosController(listaPostgresRespository);
+  const listaPostgresRepository = new ListaPostgresRepository();
+  const listacontroller = new ListaHorariosController(listaPostgresRepository);
   return new LogControllerDecorator(listacontroller);
 };
