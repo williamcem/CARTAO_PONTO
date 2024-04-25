e61b066a5c26720c67034fc73ed06910be9e1335
William <william@inovaetech.com.br>
2024-04-25 16:38:03 -0300

    alteração do main para rodar o build

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,10 +1,18 @@
 import { Express, Router } from "express";
-import fg from "fast-glob";
+
+import horarios from "../routes/horarios/horarios-routes";
+import lista from "../routes/horarios/lista-routes";
+import saldo from "../routes/horarios/saldo-routes";
+import upload from "../routes/horarios/upload-routes";
 
 export const setupRoutes = (app: Express): void => {
   const router = Router();
   app.use("/api", router);
-  fg.sync("**/src/main/routes/*/**routes.ts").map(async (file) => {
-    (await import(`../../../${file}`)).default(router);
-  });
+  horarios(router);
+  lista(router);
+  saldo(router);
+  upload(router);
+  // fg.sync("**/src/main/routes/*/**routes.ts").map(async (file) => {
+  //   (await import(`../../../${file}`)).default(router);
+  // });
 };
