ab7897a5531f89ab2ef6bf27e781fc85b81ae2dc
William <william@inovaetech.com.br>
2024-04-25 16:55:05 -0300

    exclusão de linhas desnecessárias

--- src/main/routes/horarios/upload-routes.ts
+++ src/main/routes/horarios/upload-routes.ts
@@ -1,6 +1,4 @@
-import { Response, Router } from "express";
-import { adaptRoute } from "../../adapters/express-route-adapter";
-import { makeUploadController } from "../../factories/uplod-arquivos";
+import { Router } from "express";
 import multer from "multer";
 import { processarArquivo } from "../../adapters/protheus-route-adapter";
 
