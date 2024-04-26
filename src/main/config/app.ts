7a5eb2d14ae935d08663f9f58f0e8137647eb850
William <william@inovaetech.com.br>
2024-04-26 16:39:22 -0300

    implementando o cor

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import Express from "express";
 import setupMiddlewares from "./middlewares";
 import { setupRoutes } from "./routes";
+import cors from "cors";
 
 const app = Express();
+app.use(cors());
 setupMiddlewares(app);
 setupRoutes(app);
 export default app;
