7dbff23bf51b7fa692557d0d0b62cae7204b611e
William <william@inovaetech.com.br>
2024-04-26 07:58:12 -0300

    corrigindo erro de importação

--- src/main/factories/saldo-factore.ts
+++ src/main/factories/saldo-factore.ts
@@ -1,7 +1,12 @@
 import { SaldoPostgresRepository } from "../../infra/db/postgresdb/saldoAnt/saldo-repository";
 import { SaldoController } from "../../presentation/controllers/saldo/saldo-controller";
+import { DbAddSaldoAnt } from "../../data/usecase/saldoAnt/db-add-saldo-ant";
+import { Controller } from "../../presentation/protocols";
+import { LogControllerDecorator } from "../decorators/log";
 
-export const makeSaldoController = (): SaldoController => {
+export const makeSaldoController = (): Controller => {
   const saldoPostgresRepository = new SaldoPostgresRepository();
-  return new SaldoController(saldoPostgresRepository);
+  const dbAddSaldoAnt = new DbAddSaldoAnt(saldoPostgresRepository);
+  const saldoController = new SaldoController(dbAddSaldoAnt);
+  return new LogControllerDecorator(saldoController);
 };
