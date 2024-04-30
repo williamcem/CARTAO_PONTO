5b2b48debf0638ba2bad3a2ab572d6f434a10b68
William <william@inovaetech.com.br>
2024-04-30 11:29:28 -0300

    adição da rota deletar

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -4,6 +4,7 @@ import horarios from "../routes/horarios/horarios-routes";
 import lista from "../routes/horarios/lista-routes";
 import saldo from "../routes/horarios/saldo-routes";
 import upload from "../routes/horarios/upload-routes";
+import deletar from "../routes/horarios/delete-routes";
 
 export const setupRoutes = (app: Express): void => {
   const router = Router();
@@ -12,4 +13,5 @@ export const setupRoutes = (app: Express): void => {
   lista(router);
   saldo(router);
   upload(router);
+  deletar(router);
 };
