e5ed395dd51041f584bf96d2c01b4bdbaa376260
William <william@inovaetech.com.br>
2024-04-30 11:31:33 -0300

    rota para a função deletar

---
+++ src/main/routes/horarios/delete-routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { adaptRoute } from "../../adapters/express-route-adapter";
+import { makeDeleteController } from "../../factories/delete-factore";
+
+// Rota para deletar dados
+const route = (router: Router): void => {
+  router.delete("/deletar", adaptRoute(makeDeleteController()));
+};
+
+export default route;
