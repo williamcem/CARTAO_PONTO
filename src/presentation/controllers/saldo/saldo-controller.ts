0dcb9e5327c2372f53e9bf0885b612ab51f22e23
William <william@inovaetech.com.br>
2024-05-06 15:43:01 -0300

    verificação da regra dos 10 min e filtro de data atual com as passadas

--- src/presentation/controllers/saldo/saldo-controller.ts
+++ src/presentation/controllers/saldo/saldo-controller.ts
@@ -1,7 +1,8 @@
 import { PrismaClient } from "@prisma/client";
-import { HttpRequest, HttpResponse, Controller } from "./saldo-protocols";
+import { HttpResponse, Controller } from "./saldo-protocols";
 import { DbAddSaldoAnt } from "../../../data/usecase/saldoAnt/db-add-saldo-ant";
 import { serverError, ok } from "../../../presentation/helpers/http-helpers";
+import { prisma } from "../../../infra/database/Prisma";
 
 export class SaldoController implements Controller {
   private readonly dbAddSaldoAnt: DbAddSaldoAnt;
@@ -10,33 +11,54 @@ export class SaldoController implements Controller {
     this.dbAddSaldoAnt = dbAddSaldoAnt;
   }
 
-  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+  async handle(): Promise<HttpResponse> {
     try {
-      const prismaClient = new PrismaClient();
+      const prismaClient = prisma;
       const dias = await prismaClient.dia.findMany({
         include: { receberdados: true },
         orderBy: { receberdados: { data: "asc" } },
       });
 
-      let saldoAtual = 0;
-      for (const dia of dias) {
-        let dif_min = dia.dif_min;
+      let saldoAnterior = 0; // Inicializa o saldoAnt do primeiro dia
 
-        if (saldoAtual > 0) {
-          if (dif_min < 0) dif_min = dif_min / 1.6;
+      let contador = 0;
+      const date = new Date();
+      date.setHours(20);
+      date.setMinutes(59);
+      date.setSeconds(59);
+
+      for (const dia of dias) {
+        if (date.getTime() < dia.receberdados.data.getTime()) continue;
+
+        let dif_min = dia.dif_min;
+        if (contador === 0) saldoAnterior = dia.receberdados.saldoanterior;
+
+        // Verifica se deve dividir dif_min por 1.6
+        if (saldoAnterior > 0 && dif_min < 0) {
+          dif_min = dif_min / 1.6;
         }
-        saldoAtual = saldoAtual + dif_min;
+
+        // Verifica se dif_min está no intervalo de -10 e 10
+        if (dif_min >= -10 && dif_min <= 10) {
+          // Não faz nenhuma alteração no saldoAnt
+          dif_min = 0;
+        }
+
+        // Atualiza o saldoAtual com base no saldoAnterior
+        let saldoAtual = saldoAnterior + dif_min;
 
         // Arredonda a parte decimal de saldoAtual
         saldoAtual = arredondarParteDecimal(saldoAtual);
 
-        // Verificar se dif_min está entre -10 e 10
-        if (dif_min >= -10 && dif_min <= 10) {
-          continue; // Pula para a próxima iteração do loop
-        }
+        // Atualiza o saldoAnt apenas se dif_min estiver fora do intervalo
+        await prismaClient.dia.update({
+          where: { id: dia.id },
+          data: { saldoAnt: saldoAtual }, // Atualiza o saldoAnt
+        });
 
-        const saved = await prismaClient.dia.update({ where: { id: dia.id }, data: { saldoAnt: saldoAtual } });
-        console.log(saldoAtual);
+        // Atualiza o saldoAnterior para o próximo dia
+        saldoAnterior = saldoAtual;
+        contador++;
       }
 
       return ok({ message: "Saldo anterior calculado e adicionado com sucesso" });
