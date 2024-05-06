fbcfee579852c6df0ef05f010cd6456c4658b4cb
William <william@inovaetech.com.br>
2024-05-06 15:41:48 -0300

    tirando parametros não pertinentes ao teste

--- src/infra/db/postgresdb/horarios-repository/horarios.spec.ts
+++ src/infra/db/postgresdb/horarios-repository/horarios.spec.ts
@@ -24,14 +24,12 @@ describe("Horarios Postgres Repository", () => {
   test("Deve retornar uma semana de trabalho em caso de sucesso", async () => {
     const HorariosData: AddHorariosModel = {
       id: randomUUID(),
-      data: "05/04/2024",
       entradaManha: "07:12",
       saidaManha: "09:00",
       entradaTarde: "10:00",
       saidaTarde: "11:00",
       entradaExtra: "12:00",
       saidaExtra: "17:00",
-      saldoAnt: 300,
       dif_min: 1,
     };
     const insertdHorarios = await sut.add(HorariosData);
@@ -44,7 +42,6 @@ describe("Horarios Postgres Repository", () => {
     expect(insertdHorarios.saidaTarde).toBe(HorariosData.saidaTarde);
     expect(insertdHorarios.entradaExtra).toBe(HorariosData.entradaExtra);
     expect(insertdHorarios.saidaExtra).toBe(HorariosData.saidaExtra);
-    expect(insertdHorarios.saldoAnt).toBe(HorariosData.saldoAnt);
     expect(insertdHorarios.dif_min).toBe(HorariosData.dif_min);
   });
 });
