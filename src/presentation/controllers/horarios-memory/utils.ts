43648dc2da2405fb35ac8d8875c324405ff0eb28
William <william@inovaetech.com.br>
2024-05-10 07:45:30 -0300

    arquivo para funçãos de calculos em memoria

---
+++ src/presentation/controllers/horarios-memory/utils.ts
@@ -0,0 +1,27 @@
+export function calcularTotalMinutos(entrada: string, saida: string, extra?: string): number {
+  const [entradaHoras, entradaMinutos] = entrada.split(":").map(Number);
+  const [saidaHoras, saidaMinutos] = saida.split(":").map(Number);
+
+  let totalMinutosEntrada = entradaHoras * 60 + entradaMinutos;
+  let totalMinutosSaida = saidaHoras * 60 + saidaMinutos;
+
+  if (extra && extra !== "") {
+    const [extraHoras, extraMinutos] = extra.split(":").map(Number);
+    totalMinutosSaida += extraHoras * 60 + extraMinutos;
+  }
+  /* console.log("bateu", totalMinutosSaida, totalMinutosEntrada); */
+  return totalMinutosSaida - totalMinutosEntrada;
+
+}
+
+export function arredondarParteDecimal(numero: number): number {
+  const inteiro = Math.floor(numero); // Obtém a parte inteira do número
+  const decimal = numero - inteiro; // Obtém a parte decimal do número
+
+  // Se a parte decimal for maior que 0.5, arredonde para cima
+  if (decimal > 0.5) {
+    return inteiro + 1; // Adiciona 1 para arredondar para cima
+  } else {
+    return inteiro; // Mantém o inteiro, arredondando para baixo
+  }
+}
