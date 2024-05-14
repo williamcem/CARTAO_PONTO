db77e9adf54b808ba222060dce84f3de34760c23
William <william@inovaetech.com.br>
2024-05-14 16:57:23 -0300

    alteração para retorno da porta em que o server está rodando

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 import "dotenv/config";
 import app from "./main/config/app";
 
-app.listen(process.env.PORT, () => console.log(`Server rodando em http://localhost:${process.env.PORT}`));
+app.listen(Number(process.env.PORT), "0.0.0.0", () => console.log(`Server rodando em http://localhost:${process.env.PORT}`));
