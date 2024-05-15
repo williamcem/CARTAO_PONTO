ecb451b91c0841aa2b80edd5c504809d8ace002b
William <william@inovaetech.com.br>
2024-05-15 13:48:25 -0300

    correção de ortografia

--- src/presentation/errors/extra-erro.ts
+++ src/presentation/errors/extra-erro.ts
@@ -1,6 +1,6 @@
 export class ExtraParamError extends Error {
   constructor(paramName: string) {
     super("Se entradaExtra ou saidaExtra for fornecidos, ambos devem estar presentes: ${paramName}");
-    this.name = "Se entradaExtra ou saidaExtra for fornecidos, ambos devem estar presentes";
+    this.name = "Se entrada Extra ou saida Extra for fornecidos, ambos devem estar presentes";
   }
 }
--- src/presentation/errors/manha-erro.ts
+++ src/presentation/errors/manha-erro.ts
@@ -1,6 +1,6 @@
 export class ManhaParamError extends Error {
   constructor(paramName: string) {
     super("Se entradaManha ou saidaManha for fornecidos, ambos devem estar presentes: ${paramName}");
-    this.name = "Se entradaTarde ou saidaTarde for fornecidos, ambos devem estar presentes";
+    this.name = "Se entrada Manha ou saida Manha for fornecidos, ambos devem estar presentes";
   }
 }
