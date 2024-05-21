e8a78578ad882b6dd1bd790ffa35d8c30a9e6cb3
William <william@inovaetech.com.br>
2024-05-21 09:17:39 -0300

    rota para busca de funcionário

---
+++ src/main/routes/horarios/get-funcionario.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { adaptRoute } from "../../adapters/express-route-adapter";
+import { makeGetFuncionarioController } from "../../factories/get-funcionario";
+
+const route = (router: Router): void => {
+  router.get("/funcionario", adaptRoute(makeGetFuncionarioController()));
+};
+
+export default route;
