96d75971fe7d8207343ba77f14808246d33711e3
William <william@inovaetech.com.br>
2024-05-21 10:43:05 -0300

    alteração do caminho da importação

--- src/presentation/controllers/horarios-memory/horarios-memory-protocols.ts
+++ src/presentation/controllers/horarios-memory/horarios-memory-protocols.ts
@@ -1,3 +1,3 @@
 export * from "../../protocols";
 export * from "../../../domain/usecases/add-horarios-memory";
-export * from "../../../domain/models/horariosMemory";
+export * from "../../../domain/models/horarios-memory";
