2d49d8ef51bacec888b620c3c015a1943116fcd7
William <william@inovaetech.com.br>
2024-05-22 16:11:05 -0300

    Adicionad todos funcionarios

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,13 +1,16 @@
 import { Express, Router } from "express";
 
+import todosfuncionarios from "../routes/horarios/buscar-todos-funcionarios";
+import deletar from "../routes/horarios/delete-routes";
+import falta from "../routes/horarios/dif-min-routes";
+import funcionario from "../routes/horarios/get-funcionario";
 import horarios from "../routes/horarios/horarios-routes";
+import horariosMemory from "../routes/horarios/horraio-memory-routes";
 import lista from "../routes/horarios/lista-routes";
 import saldo from "../routes/horarios/saldo-routes";
 import upload from "../routes/horarios/upload-routes";
-import deletar from "../routes/horarios/delete-routes";
-import falta from "../routes/horarios/dif-min-routes";
-import horariosMemory from "../routes/horarios/horraio-memory-routes";
-import funcionario from "../routes/horarios/get-funcionario";
+/* import uploadfuncionario from "../routes/horarios/upload-routes";
+import uploadcartao from "../routes/horarios/upload-routes"; */
 
 export const setupRoutes = (app: Express): void => {
   const router = Router();
@@ -20,4 +23,5 @@ export const setupRoutes = (app: Express): void => {
   falta(router);
   horariosMemory(router);
   funcionario(router);
+  todosfuncionarios(router);
 };
