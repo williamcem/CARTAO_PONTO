82a83edd073cd66e84b55c720c31ec74009b8993
William <william@inovaetech.com.br>
2024-05-22 10:11:23 -0300

    adicionado retorno not found request

--- src/presentation/helpers/http-helpers.ts
+++ src/presentation/helpers/http-helpers.ts
@@ -2,6 +2,11 @@ import { HttpResponse } from "../protocols/http";
 import { ServerError } from "../errors/server-error";
 
 export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error,
+});
+
+export const notFoundRequest = (error: Error): HttpResponse => ({
   statusCode: 404,
   body: error,
 });
