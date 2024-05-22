cfca2c812f01427ed31d63c8c7e9674d68763b12
William <william@inovaetech.com.br>
2024-05-22 16:10:46 -0300

    adicionado quer e params na requisicao

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -6,6 +6,8 @@ export const adaptRoute = (controller: Controller) => {
   return async (req: Request, res: Response) => {
     const HttpRequest: HttpRequest = {
       body: req.body,
+      query: req.query,
+      params: req.params,
     };
     const httpResponse = await controller.handle(HttpRequest);
     res.status(httpResponse.statusCode).json(httpResponse.body);
