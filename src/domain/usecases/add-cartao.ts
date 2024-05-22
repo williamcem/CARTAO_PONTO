fd5f735eb425f29f53f429b13dfaf277dae6a1c5
William <william@inovaetech.com.br>
2024-05-22 16:13:57 -0300

    upload horario

---
+++ src/domain/usecases/add-cartao.ts
@@ -0,0 +1,27 @@
+import { CartaoUpsertModel } from "../models/cartao"
+
+export interface AddCartaoUpsertModel {
+  identificacao: string;
+  funcionarioId: number;
+  referencia: Date;
+  saldoAnterior60: number;
+  saldoAnterior100: number;
+  status: { id: number; descricao: string };
+  dias: {
+    data: Date;
+    periodoDescanso: number;
+    cargaHor: number;
+    cargaHorPrimeiroPeriodo: number;
+    cargaHorSegundoPeriodo: number;
+    cargaHorariaCompleta: string;
+    cargaHorNoturna: number;
+    status: {
+      id: number;
+      descricao: string;
+    };
+  }[];
+}
+
+export interface AddCartoes {
+  upsert(input: CartaoUpsertModel): Promise<boolean>;
+}
