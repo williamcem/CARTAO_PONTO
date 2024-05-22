b762605cf8a933fb745f429ee58ca1dad1031c77
William <william@inovaetech.com.br>
2024-05-22 16:10:27 -0300

    Arrumado mesagem de erro

--- src/presentation/errors/Funcionario-param-error.ts
+++ src/presentation/errors/Funcionario-param-error.ts
@@ -1,6 +1,6 @@
 export class FuncionarioParamError extends Error {
   constructor(paramName: string) {
-    super("Identificador não encontrado: ${paramName}");
-    this.name = "Identificador não encontrado";
+    super();
+    this.name = paramName;
   }
 }
