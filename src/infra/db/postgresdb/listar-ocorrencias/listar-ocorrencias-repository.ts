2f4e412d2d9a97dadd2f08fe3969b69d16e29f4c
William <william@inovaetech.com.br>
2024-06-03 07:52:46 -0300

    repositorio para listar ocorrencias

---
+++ src/infra/db/postgresdb/listar-ocorrencias/listar-ocorrencias-repository.ts
@@ -0,0 +1,48 @@
+import { PrismaClient } from "@prisma/client";
+import { ListarOcorrencias } from "../../../../data/usecase/listar-ocorrencias/add-listar-ocorrencias";
+import { prisma } from "../../../database/Prisma";
+
+export class OcorrenciaPostgresRepository implements ListarOcorrencias {
+  private prisma: PrismaClient;
+
+  constructor() {
+    this.prisma = prisma;
+  }
+
+  public async find(localidade: string): Promise<{ funcionarios: { identificacao: string; cartao: any }[] }> {
+    const funcionarios = await this.prisma.funcionario.findMany({
+      where: { localidadeId: localidade },
+      include: {
+        cartao: {
+          include: {
+            cartao_dia: {
+              include: {
+                cartao_dia_lancamentos: {
+                  include: {
+                    cartao_dia_lancamento_status: true,
+                  },
+                },
+                cartao_dia_status: true,
+              },
+              orderBy: { id: "asc" },
+            },
+            cartao_status: true,
+          },
+          orderBy: { id: "asc" },
+        },
+        turno: true,
+        localidade: true,
+      },
+      orderBy: { id: "asc" },
+    });
+
+    if (!funcionarios) return { funcionarios: [] };
+
+    return {
+      funcionarios: funcionarios.map((funcionario) => ({
+        identificacao: funcionario.identificacao,
+        cartao: funcionario.cartao,
+      })),
+    };
+  }
+}
