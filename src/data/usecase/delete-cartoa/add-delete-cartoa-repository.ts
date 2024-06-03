5e22baf6f4c0826edc265c6050b383e8423d93a6
William <william@inovaetech.com.br>
2024-06-03 07:46:56 -0300

    alteração de importação

--- src/data/usecase/delete-cartoa/add-delete-cartoa-repository.ts
+++ src/data/usecase/delete-cartoa/add-delete-cartoa-repository.ts
@@ -1,5 +1,5 @@
-import { DeleteCartoaModel } from "../../../domain/models/delete-cartao"
+import { DeleteCartoaModel } from "../../../domain/models/delete-cartao";
 
 export interface DelDeleteCartoaRepository {
-  deleteByReferencia(deleteReferencia: DeleteCartoaModel): Promise<void>
+  deleteByReferencia(deleteReferencia: DeleteCartoaModel): Promise<void>;
 }
