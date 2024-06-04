c37f02665f517cd0af308c6f424b37d813df5154
William <william@inovaetech.com.br>
2024-06-04 07:31:43 -0300

    controller para listar status

---
+++ src/presentation/controllers/listar-status-lancamento/listar-status-lancamento-controler.ts
@@ -0,0 +1,19 @@
+import { ListarLancamentoRepsository } from "@infra/db/postgresdb/listar-status-lancamento-repository/listar-status-lancamento-repository";
+import { ok, serverError } from "../../helpers/http-helpers";
+import { Controller, HttpResponse } from "./listar-status-lancamnrto-protocols";
+
+export class ListarStatusController implements Controller {
+  constructor(private readonly localidadePostgresRepository: ListarLancamentoRepsository) {}
+
+  async handle(): Promise<HttpResponse> {
+    try {
+      const lancamentos = await this.localidadePostgresRepository.list();
+
+      // Envolver o resultado em um objeto chamado data
+      return ok({ data: lancamentos });
+    } catch (error) {
+      console.error(error);
+      return serverError();
+    }
+  }
+}
