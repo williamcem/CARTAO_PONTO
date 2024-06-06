68e2e779a15af522b8bad575be56a2723a948c3e
William <william@inovaetech.com.br>
2024-06-06 10:52:24 -0300

    alteração da ordem (eslint)

--- src/main/routes/horarios/upload-routes.ts
+++ src/main/routes/horarios/upload-routes.ts
@@ -1,6 +1,12 @@
 import { Router } from "express";
 import multer from "multer";
-import { processarArquivo, importarArquivoFuncionario, importarArquivoCartao } from "../../adapters/protheus-route-adapter";
+
+import {
+  importarArquivoCartao,
+  importarArquivoFuncionario,
+  importarArquivosAfastamento,
+  processarArquivo,
+} from "../../adapters/protheus-route-adapter";
 
 const upload = multer();
 
@@ -9,6 +15,8 @@ const route = (router: Router): void => {
   router.post("/upload", upload.single("arquivo"), (req, res) => processarArquivo(req, res));
   router.post("/uploadfuncionario", upload.single("arquivo"), (req, res) => importarArquivoFuncionario(req, res));
   router.post("/uploadcartao", upload.single("arquivo"), (req, res) => importarArquivoCartao(req, res));
+
+  router.post("/uploadafastamento", upload.single("arquivo"), (req, res) => importarArquivosAfastamento(req, res));
 };
 
 export default route;
