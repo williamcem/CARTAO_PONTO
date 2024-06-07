fc8f2e861e70a33a0fc4559e5d9c1b13cd761cb4
William <william@inovaetech.com.br>
2024-06-07 15:17:23 -0300

    nenhuma alteração

--- src/main/routes/horarios/export-routes.ts
+++ src/main/routes/horarios/export-routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { ExportarController } from "../../../presentation/controllers/exportar-arquivos/exporatar-lancamentos";
+import { ExportarController } from "../../../presentation/controllers/exportar-arquivos-geral/exporatar-lancamentos";
 import { adaptRoute } from "../../adapters/express-route-adapter";
 
 const route = (router: Router): void => {
