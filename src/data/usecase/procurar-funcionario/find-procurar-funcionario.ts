f52b877dd84e02c4306abd12e0a449209170ad1c
William <william@inovaetech.com.br>
2024-06-07 13:28:48 -0300

    correção (eslint)

--- src/data/usecase/procurar-funcionario/find-procurar-funcionario.ts
+++ src/data/usecase/procurar-funcionario/find-procurar-funcionario.ts
@@ -1,5 +1,6 @@
 import { GetFuncionarioModel } from "../../../presentation/controllers/procurar-funcionário/procurra-funcionario-protocols";
 
 export interface GetFuncionarioIdent {
+  list: any;
   findFisrt(funcionarioData: string, localidade: string): Promise<GetFuncionarioModel | undefined>;
 }
