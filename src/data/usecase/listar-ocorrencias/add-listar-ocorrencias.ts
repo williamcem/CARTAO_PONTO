0a304fbedbd6a39398385c8c2337eb49cff1dae6
William <william@inovaetech.com.br>
2024-07-04 16:44:52 -0300

    metodo para buscar ocorrencias com retorno so de nome e identificação

--- src/data/usecase/listar-ocorrencias/add-listar-ocorrencias.ts
+++ src/data/usecase/listar-ocorrencias/add-listar-ocorrencias.ts
@@ -1,3 +1,7 @@
 export interface ListarOcorrencias {
   find(localidade: string): Promise<{ funcionarios: { identificacao: string }[] }>;
 }
+
+export interface ListarOcorrenciasGeral {
+  findOcorrencia(localidade: string): Promise<{ funcionarios: { identificacao: string; nome: string }[] }>;
+}
