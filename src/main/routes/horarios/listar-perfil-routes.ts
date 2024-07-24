ee18964c9220a801fd0b0fabfdb1d648827e2a57
Diego <diego@inovaetech.com.br>
2024-07-24 15:08:04 -0300

    fix: alterado para get

--- src/main/routes/horarios/listar-perfil-routes.ts
+++ src/main/routes/horarios/listar-perfil-routes.ts
@@ -3,7 +3,7 @@ import { adaptRoute } from "../../adapters/express-route-adapter";
 import { makeListarPerfilController } from "../../factories/listar-perfil";
 
 const route = (router: Router): void => {
-  router.post("/listar-perfil", adaptRoute(makeListarPerfilController()));
+  router.get("/listar-perfil", adaptRoute(makeListarPerfilController()));
 };
 
 export default route;
