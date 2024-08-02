0a0b337231df440cb3de85460862e313dca7ff8c
William <william@inovaetech.com.br>
2024-08-02 15:22:30 -0300

    comentado console de vizualização da rota e requisição

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,4 +1,16 @@
- import cors from "cors";
+import cors from "cors";
+import Express from "express";
+
+import setupMiddlewares from "./middlewares";
+import { setupRoutes } from "./routes";
+
+const app = Express();
+app.use(cors());
+setupMiddlewares(app);
+setupRoutes(app);
+export default app;
+
+/* import cors from "cors";
 import Express, { NextFunction, Request, Response } from "express";
 
 import setupMiddlewares from "./middlewares";
@@ -8,11 +20,11 @@ const app = Express();
 
 app.use(cors());
 app.use((req: Request, resp: Response, next: NextFunction) => {
-   console.log("Requisição", req.url, req.query, req.body, req.ip);
+/*   console.log("Requisição", req.url, req.query, req.body, req.ip);
   next();
 });
 
 setupMiddlewares(app);
 setupRoutes(app);
 export default app;
-
+ */
