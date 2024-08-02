c9f734fea916c344df9542f5545b3d5908aaf46c
William <william@inovaetech.com.br>
2024-08-02 15:21:31 -0300

    desabilitado eslint na linha 6

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,8 @@
 import { Express } from "express";
 
-import { bodyParser, contentType,cors } from "../middlewares";
+import { bodyParser, contentType, cors } from "../middlewares";
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default (app: Express): void => {
   app.use(bodyParser);
   app.use(cors);
