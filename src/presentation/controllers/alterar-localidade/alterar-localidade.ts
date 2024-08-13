66233f76c77502e7267c2cfbaaef579d6626f6c4
Diego <diego@inovaetech.com.br>
2024-08-13 07:19:22 -0300

    feat: buscar os dias do cartao pelo informado

--- src/presentation/controllers/alterar-localidade/alterar-localidade.ts
+++ src/presentation/controllers/alterar-localidade/alterar-localidade.ts
@@ -2,15 +2,21 @@ import { AlterarLocalidadePostgresRepository } from "@infra/db/postgresdb/altera
 import { FuncionarioParamError } from "../../errors/Funcionario-param-error";
 import { badRequest, notFoundRequest, ok, serverError } from "../../helpers/http-helpers";
 import { Controller, HttpRequest, HttpResponse } from "./alterar-localidade-protocols";
+import moment from "moment";
 
 export class AlterarLocalidadeController implements Controller {
   constructor(private readonly alterarLocalidadePostgresRepository: AlterarLocalidadePostgresRepository) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const { funcionarioId, localidadeId, turnoId } = httpRequest?.body;
+      const { funcionarioId, localidadeId, turnoId, inicioVigencia, fimVigencia } = httpRequest?.body;
 
       if (!funcionarioId) return badRequest(new FuncionarioParamError("Falta funcionário!"));
+      if (!inicioVigencia) return badRequest(new FuncionarioParamError("Falta inicio vigência!"));
+      if (!fimVigencia) return badRequest(new FuncionarioParamError("Falta fim vigência!"));
+
+      if (!moment(inicioVigencia).isValid()) return badRequest(new FuncionarioParamError("Início vigência não é válido!"));
+      if (!moment(fimVigencia).isValid()) return badRequest(new FuncionarioParamError("Fim vigência não é válido!"));
 
       const funcionario = await this.alterarLocalidadePostgresRepository.findFisrtFuncionario({ id: Number(funcionarioId) });
 
@@ -32,7 +38,12 @@ export class AlterarLocalidadeController implements Controller {
         if (!turno) return notFoundRequest(new FuncionarioParamError(`Turno ${localidadeId} não existe!`));
       }
 
-      console.log({ id: funcionario.id, localidadeId, turnoId: turnoId ? Number(turnoId) : undefined });
+      const dias = await this.alterarLocalidadePostgresRepository.findManyDias({
+        fim: new Date(fimVigencia),
+        inicio: new Date(inicioVigencia),
+        funcionarioId: funcionario.id,
+      });
+
       const saved = await this.alterarLocalidadePostgresRepository.updateFuncionario({
         id: funcionario.id,
         localidadeId,
