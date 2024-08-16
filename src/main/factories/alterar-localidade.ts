55b81b7ca04dc6dc4b722a243eda03010d1edd86
Diego <diego@inovaetech.com.br>
2024-08-16 07:54:43 -0300

    feat: reutilizado função para calcular minutos noturnos

--- src/main/factories/alterar-localidade.ts
+++ src/main/factories/alterar-localidade.ts
@@ -4,10 +4,14 @@ import { Controller } from "../../presentation/protocols";
 import { LogControllerDecorator } from "../decorators/log";
 import { CriarEventosController } from "../../presentation/controllers/eventos/eventos-controller";
 import { CriarEventosPostgresRepository } from "@infra/db/postgresdb/eventos/eventos-repository";
+import { RecalcularTurnoController } from "../../presentation/controllers/recalcular-turno/recalcular-turno";
+import { RecalcularTurnoPostgresRepository } from "@infra/db/postgresdb/recalcular-turno/recalcular-turno";
 
 export const makeAlterarLocalidadeontroller = (): Controller => {
   const alterarLocalidadePostgresRepository = new AlterarLocalidadePostgresRepository();
-  const criarEventosPostgresRepository = new CriarEventosPostgresRepository();
+  const recalcularTurnoPostgresRepository = new RecalcularTurnoPostgresRepository();
+  const recalcularTurnoController = new RecalcularTurnoController(recalcularTurnoPostgresRepository);
+  const criarEventosPostgresRepository = new CriarEventosPostgresRepository(recalcularTurnoController);
   const criarEventosController = new CriarEventosController(criarEventosPostgresRepository);
   const alterarLocalidadeController = new AlterarLocalidadeController(
     alterarLocalidadePostgresRepository,
