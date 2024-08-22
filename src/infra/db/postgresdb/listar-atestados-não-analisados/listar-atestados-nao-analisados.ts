fcfa64dcaa09d6ada2b31d3744bc4d328d75faa0
Diego <diego@inovaetech.com.br>
2024-08-22 15:44:31 -0300

    feat: retorna cartao id pela referencia se houver

--- src/infra/db/postgresdb/listar-atestados-não-analisados/listar-atestados-nao-analisados.ts
+++ src/infra/db/postgresdb/listar-atestados-não-analisados/listar-atestados-nao-analisados.ts
@@ -3,14 +3,14 @@ import { PrismaClient } from "@prisma/client";
 import { ListarAtestado } from "../../../../data/usecase/add-listar-atestados/add-listar-atestados";
 import { prisma } from "../../../database/Prisma";
 
-export class ListarAtestadoRepsository implements ListarAtestado {
+export class ListarAtestadoRepsository {
   private prisma: PrismaClient;
 
   constructor() {
     this.prisma = prisma;
   }
 
-  public async list(): Promise<any[]> {
+  public async list(input: { referencia: Date }): Promise<any[]> {
     const atestados = await this.prisma.atestado_funcionario.findMany({
       where: {
         tipo_status: {
@@ -18,7 +18,7 @@ export class ListarAtestadoRepsository implements ListarAtestado {
         },
       },
       include: {
-        funcionario: true,
+        funcionario: { include: { cartao: { select: { id: true }, where: { referencia: input.referencia } } } },
         tipo_acompanhante: true,
         tipo_ocupacao: true,
         tipo_status: true,
@@ -34,37 +34,42 @@ export class ListarAtestadoRepsository implements ListarAtestado {
       },
     });
 
-    return atestados.map((atestado) => ({
-      data: atestado.data,
-      id: atestado.id,
-      inicio: atestado.inicio,
-      fim: atestado.fim,
-      grupo_cid: atestado.grupo_cid,
-      acidente_trabalho: atestado.acidente_trabalho,
-      descricao: atestado.descricao,
-      userName: atestado.userName,
-      funcionarioId: atestado.funcionarioId,
-      idade_paciente: atestado.idade_paciente,
-      trabalhou_dia: atestado.trabalhou_dia,
-      observacao: atestado.observacao,
-      nome: atestado.nomeFuncionario,
-      exame: atestado.exame,
-      nome_acompanhante: atestado.nome_acompanhante,
-      identificacao: atestado.funcionario?.identificacao,
-      sintomas: atestado.sintomas,
-      nomeAcao: atestado.tipo_eventos?.nome,
-      nomeAcompanhante: atestado.tipo_acompanhante?.nome,
-      nomeOcupacao: atestado.tipo_ocupacao?.nome,
-      nomeStatus: atestado.tipo_status?.nome,
-      nomeDocumento: atestado.tipos_documentos?.nome,
-      nomeComprovante: atestado.tipo_comprovante_ausencia?.nome,
-      nomeCertidao: atestado.tipo_certidao_obito?.nome,
-      funcao: atestado.funcao.nome,
-      horarios: atestado.entradas_saidas_atestado.map((valor) => ({
-        entrada: valor.entrada,
-        saida: valor.saida,
-      })),
-      localidadeId: atestado.funcionario.localidadeId,
-    }));
+    return atestados.map((atestado) => {
+      const cartaoId = atestado.funcionario.cartao.length ? atestado.funcionario.cartao[0].id : undefined;
+
+      return {
+        data: atestado.data,
+        id: atestado.id,
+        inicio: atestado.inicio,
+        fim: atestado.fim,
+        grupo_cid: atestado.grupo_cid,
+        acidente_trabalho: atestado.acidente_trabalho,
+        descricao: atestado.descricao,
+        userName: atestado.userName,
+        funcionarioId: atestado.funcionarioId,
+        idade_paciente: atestado.idade_paciente,
+        trabalhou_dia: atestado.trabalhou_dia,
+        observacao: atestado.observacao,
+        nome: atestado.nomeFuncionario,
+        exame: atestado.exame,
+        nome_acompanhante: atestado.nome_acompanhante,
+        identificacao: atestado.funcionario?.identificacao,
+        sintomas: atestado.sintomas,
+        nomeAcao: atestado.tipo_eventos?.nome,
+        nomeAcompanhante: atestado.tipo_acompanhante?.nome,
+        nomeOcupacao: atestado.tipo_ocupacao?.nome,
+        nomeStatus: atestado.tipo_status?.nome,
+        nomeDocumento: atestado.tipos_documentos?.nome,
+        nomeComprovante: atestado.tipo_comprovante_ausencia?.nome,
+        nomeCertidao: atestado.tipo_certidao_obito?.nome,
+        funcao: atestado.funcao.nome,
+        horarios: atestado.entradas_saidas_atestado.map((valor) => ({
+          entrada: valor.entrada,
+          saida: valor.saida,
+        })),
+        localidadeId: atestado.funcionario.localidadeId,
+        cartaoId,
+      };
+    });
   }
 }
--- src/presentation/controllers/listar-atestados-não-analisados/listar-atestados-nao-analisados-controler.ts
+++ src/presentation/controllers/listar-atestados-não-analisados/listar-atestados-nao-analisados-controler.ts
@@ -1,14 +1,22 @@
 import { ListarAtestadoRepsository } from "@infra/db/postgresdb/listar-atestados-não-analisados/listar-atestados-nao-analisados";
 
-import { ok, serverError } from "../../helpers/http-helpers";
-import { Controller, HttpResponse } from "./listar-atestados-nao-analisados-protocols";
+import { badRequestNovo, ok, serverError } from "../../helpers/http-helpers";
+import { Controller, HttpRequest, HttpResponse } from "./listar-atestados-nao-analisados-protocols";
+import moment from "moment";
 
 export class ListarAtestadoController implements Controller {
   constructor(private readonly AtestadoPostgresRepository: ListarAtestadoRepsository) {}
 
-  async handle(): Promise<HttpResponse> {
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const atestados = await this.AtestadoPostgresRepository.list();
+      const { referencia } = httpRequest.query;
+
+      if (!referencia) return badRequestNovo({ message: "Falta referência!" });
+
+      if (!moment(referencia).isValid()) return badRequestNovo({ message: "Data referência inválida!" });
+
+      const atestados = await this.AtestadoPostgresRepository.list({ referencia: moment.utc(referencia).toDate() });
+
       return ok({ atestados });
     } catch (error) {
       console.error(error);
