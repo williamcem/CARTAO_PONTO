fdf4f6a1aa969f7402105ff1084c7f960ccf520f
Diego <diego@inovaetech.com.br>
2024-08-23 13:24:34 -0300

    feat: adicionado importação

--- src/main/factories/buscar-funcionario-referencia-localidade.ts
+++ src/main/factories/buscar-funcionario-referencia-localidade.ts
@@ -3,13 +3,19 @@ import { LogControllerDecorator } from "../decorators/log";
 import { BuscarFuncionarioReferenciaLocalidadeAgrupadaController } from "../../presentation/controllers/buscar-funcionarios-referencia-localidade/buscar-funcionarios-referencia-localidade";
 import { BuscarFuncionarioReferenciaLocalidadePostgresRepository } from "@infra/db/postgresdb/buscar-funcionario-referencia-localidade/buscar-funcionario-referencia-localidade";
 import { BuscarTodosPostgresRepository } from "@infra/db/postgresdb/buscar-todos-funcionarios.ts/buscas-todos-repository";
+import { FinalizarCartaoController } from "../../presentation/controllers/finalizar-cartao/finalizar-cartao";
+import { FinalizarCartaoPostgresRepository } from "@infra/db/postgresdb/finalizar-cartao/finalizar-cartao";
 
 export const makeBuscarFuncionarioReferenciaLocalidadeController = (): Controller => {
   const buscarFuncionarioReferenciaLocalidadePostgresRepository = new BuscarFuncionarioReferenciaLocalidadePostgresRepository();
   const buscarTodosPostgresRepository = new BuscarTodosPostgresRepository();
+  const finalizarCartaoPostgresRepository = new FinalizarCartaoPostgresRepository();
+  const finalizarCartaoController = new FinalizarCartaoController(finalizarCartaoPostgresRepository);
   const buscarFuncionarioReferenciaLocalidadeAgrupadaController = new BuscarFuncionarioReferenciaLocalidadeAgrupadaController(
     buscarFuncionarioReferenciaLocalidadePostgresRepository,
     buscarTodosPostgresRepository,
+    finalizarCartaoController,
+    finalizarCartaoPostgresRepository,
   );
   return new LogControllerDecorator(buscarFuncionarioReferenciaLocalidadeAgrupadaController);
 };
