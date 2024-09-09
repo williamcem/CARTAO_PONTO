91628b21f58f48c81facfb00ef2d37ce1c3ad105
William <william@inovaetech.com.br>
2024-09-09 09:06:08 -0300

    adicionado a compensação de eventos automatico (Testar)

--- src/main/factories/criar-eventos.ts
+++ src/main/factories/criar-eventos.ts
@@ -1,15 +1,20 @@
+import { CompensacaoEventoRepository } from "@infra/db/postgresdb/compensacao-eventos-automaticos-repository/compensacao-eventos-automaticos-repository";
 import { CriarEventosPostgresRepository } from "@infra/db/postgresdb/eventos/eventos-repository";
+import { RecalcularTurnoPostgresRepository } from "@infra/db/postgresdb/recalcular-turno/recalcular-turno";
 
 import { CriarEventosController } from "../../presentation/controllers/eventos/eventos-controller";
+import { RecalcularTurnoController } from "../../presentation/controllers/recalcular-turno/recalcular-turno";
 import { Controller } from "../../presentation/protocols";
 import { LogControllerDecorator } from "../decorators/log";
-import { RecalcularTurnoController } from "../../presentation/controllers/recalcular-turno/recalcular-turno";
-import { RecalcularTurnoPostgresRepository } from "@infra/db/postgresdb/recalcular-turno/recalcular-turno";
 
 export const makeCriarEventosController = (): Controller => {
   const recalcularTurnoPostgresRepository = new RecalcularTurnoPostgresRepository();
+  const compensacaoEventoRepository = new CompensacaoEventoRepository();
   const recalcularTurnoController = new RecalcularTurnoController(recalcularTurnoPostgresRepository);
-  const criarEventosPostgresRepository = new CriarEventosPostgresRepository(recalcularTurnoController);
+  const criarEventosPostgresRepository = new CriarEventosPostgresRepository(
+    recalcularTurnoController,
+    compensacaoEventoRepository,
+  );
   const criarEventosController = new CriarEventosController(criarEventosPostgresRepository);
   return new LogControllerDecorator(criarEventosController);
 };
