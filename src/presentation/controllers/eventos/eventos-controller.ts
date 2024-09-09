a8572daa28e3c837ecd827fa12080fd86eaba95d
William <william@inovaetech.com.br>
2024-09-09 09:13:28 -0300

    adapatdo o controoler para o novo metodo de lançar dia

--- src/presentation/controllers/eventos/eventos-controller.ts
+++ src/presentation/controllers/eventos/eventos-controller.ts
@@ -1,15 +1,26 @@
 import { CriarEventosPostgresRepository } from "../../../infra/db/postgresdb/eventos/eventos-repository";
-import { ok, serverError } from "../../helpers/http-helpers";
+import { badRequest, ok, serverError } from "../../helpers/http-helpers";
 import { Controller, HttpRequest, HttpResponse } from "./eventos-protocols";
 
-export class CriarEventosController implements Controller {
+export class CriarEventosControllerNovo implements Controller {
   constructor(private readonly criarEventosPostgresRepository: CriarEventosPostgresRepository) {}
 
   async handle(req: HttpRequest): Promise<HttpResponse> {
     try {
-      const eventosCriados = await this.criarEventosPostgresRepository.add({ identificacao: req.query.identificacao });
+      const { cartao_dia_id } = req.body;
 
-      if (!eventosCriados) throw "Erro ao criar eventos!";
+      // Verificar se o cartao_dia_id está presente
+      if (!cartao_dia_id) {
+        return badRequest(new Error("Falta o parâmetro cartao_dia_id!"));
+      }
+
+      // Criar os eventos passando o identificador corretamente do cartão dia correspondente
+      const eventosCriados = await this.criarEventosPostgresRepository.add({ identificacao: String(cartao_dia_id) });
+
+      // Verificar se os eventos foram criados com sucesso
+      if (!eventosCriados) {
+        throw new Error("Erro ao criar eventos!");
+      }
 
       return ok({ message: "Eventos criados com sucesso" });
     } catch (error) {
