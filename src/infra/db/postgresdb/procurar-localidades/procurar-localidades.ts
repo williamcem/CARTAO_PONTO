52c48fdfedd0b2ffc167e06c7a441de39f34e3ae
Diego <diego@inovaetech.com.br>
2024-09-10 12:08:08 -0300

    feat: filtrar localidade pelo grupo

--- src/infra/db/postgresdb/procurar-localidades/procurar-localidades.ts
+++ src/infra/db/postgresdb/procurar-localidades/procurar-localidades.ts
@@ -3,14 +3,16 @@ import { PrismaClient } from "@prisma/client";
 import { ProcurarLocalidadeIdent } from "../../../../data/usecase/procurar-funcionario/procurar-localidades";
 import { prisma } from "../../../database/Prisma";
 
-export class LocalidadePostgresRepository implements ProcurarLocalidadeIdent {
+export class LocalidadePostgresRepository {
   private prisma: PrismaClient;
 
   constructor() {
     this.prisma = prisma;
   }
 
-  public async findMany(): Promise<{ codigo: string; nome: string }[]> {
-    return await this.prisma.localidade.findMany();
+  public async findMany(input?: { not?: { groupId?: number } }) {
+    return await this.prisma.localidade.findMany({
+      orderBy: { codigo: "asc" },
+    });
   }
 }
--- src/presentation/controllers/procurar-localidades/procurar-localidades.ts
+++ src/presentation/controllers/procurar-localidades/procurar-localidades.ts
@@ -1,14 +1,29 @@
 import { LocalidadePostgresRepository } from "@infra/db/postgresdb/procurar-localidades/procurar-localidades";
 
 import { ok, serverError } from "../../helpers/http-helpers";
-import { Controller, HttpResponse } from "./procurra-funcionario-protocols";
+import { Controller, HttpRequest, HttpResponse } from "./procurra-funcionario-protocols";
 
 export class ProcurarLocalidadeController implements Controller {
   constructor(private readonly localidadePostgresRepository: LocalidadePostgresRepository) {}
 
-  async handle(): Promise<HttpResponse> {
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      return ok(await this.localidadePostgresRepository.findMany());
+      const {
+        notGroupId,
+      }: {
+        notGroupId: number;
+      } = httpRequest.query;
+
+      const localidades = await this.localidadePostgresRepository.findMany({
+        not: { groupId: notGroupId ? Number(notGroupId) : undefined },
+      });
+
+      const output = localidades.filter((localidade) => {
+        if (!notGroupId) return localidade;
+
+        if (localidade.grupoLocalidadeId !== Number(notGroupId)) return localidade;
+      });
+      return ok(output);
     } catch (error) {
       console.error(error);
       return serverError();
