3bec5913865f1ad37f5db2c34cb5425f2606b66a
Diego <diego@inovaetech.com.br>
2024-09-24 10:15:54 -0300

    feat: importador recalcular turno controller

--- src/main/factories/buscar-funcionario-referencia-localidade.ts
+++ src/main/factories/buscar-funcionario-referencia-localidade.ts
@@ -7,14 +7,18 @@ import { FinalizarCartaoController } from "../../presentation/controllers/finali
 import { FinalizarCartaoPostgresRepository } from "@infra/db/postgresdb/finalizar-cartao/finalizar-cartao";
 import { GetFuncionarioImpressaoCalculoController } from "../../presentation/controllers/get-funcionário-impressao-calculo/procurar-funcionário-impressao-calculo";
 import { FuncionarioImpressaoCalculoPostgresRepository } from "@infra/db/postgresdb/get-funcionario-impressao-calculo/get-funcionario-impressao-calculo";
+import { RecalcularTurnoController } from "../../presentation/controllers/recalcular-turno/recalcular-turno";
+import { RecalcularTurnoPostgresRepository } from "@infra/db/postgresdb/recalcular-turno/recalcular-turno";
 
 export const makeBuscarFuncionarioReferenciaLocalidadeController = (): Controller => {
   const buscarFuncionarioReferenciaLocalidadePostgresRepository = new BuscarFuncionarioReferenciaLocalidadePostgresRepository();
   const buscarTodosPostgresRepository = new BuscarTodosPostgresRepository();
   const funcionarioImpressaoCalculoPostgresRepository = new FuncionarioImpressaoCalculoPostgresRepository();
-
+  const recalcularTurnoPostgresRepository = new RecalcularTurnoPostgresRepository();
+  const recalcularTurnoController = new RecalcularTurnoController(recalcularTurnoPostgresRepository);
   const getFuncionarioImpressaoCalculoController = new GetFuncionarioImpressaoCalculoController(
     funcionarioImpressaoCalculoPostgresRepository,
+    recalcularTurnoController,
   );
   const finalizarCartaoPostgresRepository = new FinalizarCartaoPostgresRepository();
   const finalizarCartaoController = new FinalizarCartaoController(
