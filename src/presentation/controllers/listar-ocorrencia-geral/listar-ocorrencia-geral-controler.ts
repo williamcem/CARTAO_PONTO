26171c02be212aaee1b4a5676ee04c19985d85ee
William <william@inovaetech.com.br>
2024-09-26 07:38:26 -0300

    :ok_hand: fix: em ocorrencias agora quando houver duas do tipoId 8 e solucionar uma a outra não irá sumir. Antes sumia uma se aplicasse alguma correção na outra.

--- src/presentation/controllers/listar-ocorrencia-geral/listar-ocorrencia-geral-controler.ts
+++ src/presentation/controllers/listar-ocorrencia-geral/listar-ocorrencia-geral-controler.ts
@@ -10,19 +10,30 @@ export class OcorrenciaGeralController implements Controller {
     try {
       const { localidade, referencia } = httRequest?.query;
 
-      if (!localidade) return badRequest(new Error("Localidade não informada"));
+      if (!localidade) {
+        return badRequest(new Error("Localidade não informada"));
+      }
 
-      if (!referencia) return badRequest(new Error("Referência não informada"));
+      if (!referencia) {
+        return badRequest(new Error("Referência não informada"));
+      }
 
       const data = await this.ocorrenciaGeralPostgresRepository.findOcorrencia(localidade, referencia);
 
-      const funcionarios: { id: number; identificacao: string; nome: string }[] = [];
+      const funcionarios: {
+        id: number;
+        identificacao: string;
+        nome: string;
+        eventosNaoTratados: { id: number; tratado: boolean }[];
+      }[] = [];
 
       data.map((funcionario) => {
         funcionario.cartao.map((cartao) => {
           cartao.cartao_dia.map((dia) => {
             let adicionar = false;
+            let eventosNaoTratados: { id: number; tratado: boolean }[] = [];
 
+            // Verifica se existem lançamentos não validados
             if (dia.cartao_dia_lancamentos.length !== 0) {
               const naoValidado = dia.cartao_dia_lancamentos.find(
                 (lancamento) => lancamento.cartao_dia_id === dia.id && !lancamento.validadoPeloOperador,
@@ -33,19 +44,43 @@ export class OcorrenciaGeralController implements Controller {
 
             const ocorrencia = dia.eventos.find((evento) => evento.tipoId === 2);
 
-            if (ocorrencia) adicionar = true;
+            if (ocorrencia) {
+              adicionar = true;
+            }
 
+            // Se não houver ocorrencia de tipoId === 2, verifica os eventos de tipoId === 8
             if (!adicionar) {
               const intervalos = dia.eventos.filter((evento) => evento.tipoId === 8);
 
-              if (intervalos.length > 1) adicionar = true;
+              // Verifica se existem exatamente 2 eventos de tipoId === 8
+              if (intervalos.length <= 2) {
+                // Filtra os eventos que têm tratado como false
+                eventosNaoTratados = intervalos
+                  .filter((evento) => evento.tratado === false)
+                  .map((evento) => ({
+                    id: evento.cartaoDiaId,
+                    tratado: evento.tratado,
+                  }));
+
+                if (eventosNaoTratados.length > 0) {
+                  adicionar = true;
+                }
+              }
             }
 
+            // Adiciona o funcionário à lista se atender às condições
             if (adicionar) {
               const existeIndexFuncionario = funcionarios.findIndex((func) => funcionario.id === func.id);
 
-              if (existeIndexFuncionario === -1)
-                funcionarios.push({ id: funcionario.id, identificacao: funcionario.identificacao, nome: funcionario.nome });
+              if (existeIndexFuncionario === -1) {
+                funcionarios.push({
+                  id: funcionario.id,
+                  identificacao: funcionario.identificacao,
+                  nome: funcionario.nome,
+                  eventosNaoTratados,
+                });
+              } else {
+              }
             }
           });
         });
@@ -53,10 +88,10 @@ export class OcorrenciaGeralController implements Controller {
 
       return ok(funcionarios);
     } catch (error) {
+      console.error("Erro durante o processamento:", error);
       if (error instanceof OcorrenciasNull) {
         return badRequest(new FuncionarioParamError(error.message));
       }
-      console.error(error);
       return serverError();
     }
   }
